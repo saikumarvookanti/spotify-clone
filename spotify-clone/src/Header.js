134424534f1868dcefd451cbcae2a1488b26d43f
saikumarvookanti <saikumarvookanti@gmail.com>
2021-12-15 18:34:16 +0530

    logout Functionality

--- spotify-clone/src/Header.js
+++ spotify-clone/src/Header.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import './Header.css';
 import { Avatar } from '@mui/material';
 import { useDataLayerValue } from './DataLayer';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+
 
 function Header({spotify}) {
-    const [{user},dispatch] =useDataLayerValue();
+    const [{user,token},dispatch] =useDataLayerValue();
+    const [logoutShow, setlogoutShow] = useState(false);
+
+    const logOutHandler=()=>{
+            setlogoutShow(!logoutShow);
+    }
+    const logoutClick=()=>{
+        dispatch({
+            type:'SET_TOKEN',
+            token:null
+        });
+    }
 
     return (
         <div className='header'>
@@ -18,6 +32,18 @@ function Header({spotify}) {
             <div className='header_right'>
                 <Avatar  src={user?.images[0]?.url} alt={user?.disaplay_name}/>
                 <h4>{user?.display_name}</h4>
+                {
+                    !logoutShow ? <ArrowDropDownIcon onClick={logOutHandler}/> :
+                    <div className='header_right_upArrow'><ArrowDropUpIcon onClick={logOutHandler}/>
+                    <div className='header_logout'>
+                        <h3>Profile</h3>
+                        <h3 onClick={logoutClick}>Logout</h3>
+                    </div>
+                        
+                    </div>
+                    
+                }
+                
             </div>
         </div>
     )
